7da8086e549428157a141443e8cde945a49147fd
IAmPengu <giv@givp.org>
2025-03-24 14:00:21 +1100

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import { Analytics } from "@vercel/analytics/react"
 import React from 'react';
 import TimesTableGame from './TimesTableGame';
+import Footer from './Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <TimesTableGame />
+      <Footer />
       <Analytics />
     </div>
   );
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+// Footer.js
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Timestable.fun | GNU GENERAL PUBLIC LICENSE | <a href="https://github.com/givp/times-table-app" target="_blank" rel="noreferrer">Github</a></p>
+    </footer>
+  );
+}
+
+export default Footer; 
