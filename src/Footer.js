76c168c72117d6951cc0c6817b7fdf8d5e161041
Giv <giv@givp.org>
2025-07-31 13:12:28 +1000

    Updating footer

--- src/Footer.js
+++ src/Footer.js
@@ -7,8 +7,7 @@ function Footer() {
   return (
     <footer className="footer">
       <p>
-        Timestable.fun | GNU GENERAL PUBLIC LICENSE | 
-        <a href="https://github.com/givp/times-table-app" target="_blank" rel="noreferrer"> Github</a> | 
+        Timestable.fun | GNU GENERAL PUBLIC LICENSE |  
         <Link to="/privacy"> Privacy Policy</Link> | 
         <Link to="/support"> Support</Link>
       </p>
